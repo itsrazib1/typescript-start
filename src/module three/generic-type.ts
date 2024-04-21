490cc15b44da0bdfd7b6eb3c6c8d0ef1e3922b23
itsrazib1 <itsrazib1@gmail.com>
2024-04-21 12:50:34 +0600

    adde3

--- src/module three/generic-type.ts
+++ src/module three/generic-type.ts
@@ -1,12 +1,28 @@
+type GenericTuple<X,Y> = [X,Y];
+
+const relation:GenericTuple<string,string>=['Parsian','Kate Winslet'];
+
+const relationWithSalary:GenericTuple<object,string>=[{
+    name:'Parsian',
+    salary:10000000,
+},
+"Kate Winslet"
+]
+
+type GenericArray<T> = Array<T>;
 const rollNumbers:number[] = [33,34,23];
 const rollNumber2:string[] = ["33","34","23"];
 
-const rollNumbers3:Array<number> = [33,34,23];
-const rollNumber4:Array<string> = ["33","34","23"];
+const rollNumbers3:GenericArray<number> = [33,34,23];
+const rollNumber4:GenericArray<string> = ["33","34","23"];
 
-const UserNameRollNumber: Array<{name:string,roll:number}>=[
+const UserNameRollNumber: GenericArray<{name:string,roll:number}>=[
     {
         name:'mr W',
         roll:1
+    },
+    {
+        name:"mr y",
+        roll:5
     }
 ]
