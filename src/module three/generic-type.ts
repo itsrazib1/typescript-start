3005b502053e4c6d0dd0253afb36725d4945e0fa
itsrazib1 <itsrazib1@gmail.com>
2024-04-21 12:38:27 +0600

    added

---
+++ src/module three/generic-type.ts
@@ -0,0 +1,12 @@
+const rollNumbers:number[] = [33,34,23];
+const rollNumber2:string[] = ["33","34","23"];
+
+const rollNumbers3:Array<number> = [33,34,23];
+const rollNumber4:Array<string> = ["33","34","23"];
+
+const UserNameRollNumber: Array<{name:string,roll:number}>=[
+    {
+        name:'mr W',
+        roll:1
+    }
+]
